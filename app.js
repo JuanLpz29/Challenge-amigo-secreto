8072c3c2efb88cce82691e3a5c56744e87fd9ecf
Juan López <juanlpz29@outlook.com>
2025-03-16 20:11:22 -0300

    Validar si un amigo se está repitiendo

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ function agregarAmigo() {
     if (document.getElementById('amigo').value === '') {
         alert('Debes escribir el nombre de tu amigo!');
         
-    
+    }else if(amigos.includes(document.getElementById('amigo').value)){
+        alert('Este amigo ya fue agregado!');
+
     }else{
         let amigo = document.getElementById('amigo').value;
         amigos.push(amigo);
@@ -14,4 +16,10 @@ function agregarAmigo() {
     }
 }
 
-
+function mostrarAmigos() {
+    let lista = '';
+    for (i = 0; i < amigos.length; i++) {
+        lista += `<li>${amigos[i]}</li>`;
+    }
+    document.getElementById('listaAmigos').innerHTML = lista;
+}
