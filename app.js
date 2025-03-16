e283cf8a7620e0715d415038349f90af8fa73462
Juan López <juanlpz29@outlook.com>
2025-03-16 20:06:18 -0300

    Función agrega amigos y valida espacio en blanco

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 let amigos = [];
+
+//Agregamos amigos y validamos que no hayan espacios en blanco
+function agregarAmigo() {
+    if (document.getElementById('amigo').value === '') {
+        alert('Debes escribir el nombre de tu amigo!');
+        
+    
+    }else{
+        let amigo = document.getElementById('amigo').value;
+        amigos.push(amigo);
+        document.getElementById('amigo').value = '';
+        console.log(amigos);
+    }
+}
+
+
